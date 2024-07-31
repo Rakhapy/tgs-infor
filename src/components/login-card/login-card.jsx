39f009dbf713388cc3f67226b46a788ecbf292e0
Rakhapy <rakhapy@gmail.com>
2024-07-31 18:46:04 +0700

    membuat card login

--- src/components/login-card/login-card.jsx
+++ src/components/login-card/login-card.jsx
@@ -1,7 +1,16 @@
 const Logcard = () => {
     return(
         <>
-        
+        <div className="judul-atas">
+            <h1>Login</h1>
+        </div>
+
+        <div className="card-login">
+            <div className="input-login">
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+            </div>
+        </div>
         </>
     )
 }
