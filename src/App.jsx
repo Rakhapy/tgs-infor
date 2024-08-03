29b20b1aa3ca4762cb17492fbaf2cca560c093b2
Rakhapy <rakhapy@gmail.com>
2024-08-03 18:45:38 +0700

    selesai membuat reg-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css"
 import Logcard from "./components/login-card/login-card"
+import Register from "./components/login-card/register-card/"
 
 const Home = () => {
   return(
     <>
-    <h1>Hello World</h1>
     <Logcard/>
+    <Register/>
     </>
   )
 }
